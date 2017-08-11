407f1360b980fce5ee8a1c1060e0ee3f4f13d3f3
Jon Gallion <jon.gallion@daveramsey.com>
2017-08-11 12:07:31 -0500

    updated path for .rubocop.yml

--- lib/generators/ramsey_cop_generator.rb
+++ lib/generators/ramsey_cop_generator.rb
@@ -1,9 +1,7 @@
 class RamseyCopGenerator < Rails::Generators::Base
-
   source_root(File.expand_path(File.dirname(__FILE__)))
 
   def create_rubcop_yml
-    template 'rscop.yml', 'config/.rubocop.yml'
+    template "rscop.yml", ".rubocop.yml"
   end
-
 end
