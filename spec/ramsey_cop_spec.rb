6b606c6760944f01160f0d40b546e3587b755c93
Rick Peyton <rick.peyton@daveramsey.com>
2017-09-22 10:25:38 -0500

    Remove broken test

--- spec/ramsey_cop_spec.rb
+++ spec/ramsey_cop_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe RamseyCop do
   it "has a version number" do
     expect(RamseyCop::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
