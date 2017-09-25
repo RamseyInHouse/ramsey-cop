ccc108dc501044016158bfc9cc4a5e193de04048
Rick Peyton <rick.peyton@daveramsey.com>
2017-09-25 16:35:17 -0500

    Release 0.6.0

--- lib/generators/ramsey_cop_generator.rb
+++ lib/generators/ramsey_cop_generator.rb
@@ -1,5 +1,5 @@
 class RamseyCopGenerator < Rails::Generators::Base
-  source_root(File.expand_path(File.dirname(__FILE__)))
+  source_root(File.expand_path(__dir__))
 
   def create_rubcop_yml
     template "rscop.yml", ".rubocop.yml"
