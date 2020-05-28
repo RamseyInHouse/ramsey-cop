542329eb16185fd97aab607133914e5081b9fb68
Jason York <jason.york@daveramsey.com>
2020-05-28 15:58:22 -0500

    Set default for NewCops

--- spec/ramsey_cop_spec.rb
+++ spec/ramsey_cop_spec.rb
@@ -1,7 +1,12 @@
 require "spec_helper"
+require "rubocop"
 
 RSpec.describe RamseyCop do
   it "has a version number" do
     expect(RamseyCop::VERSION).not_to be nil
+  end
+
+  it "has a valid config" do
+    expect { RuboCop::ConfigLoader.load_file("./default.yml") }.to_not raise_error
   end
 end
